0845c9334070556966df72215b5a069b1ae773dd
Donggeon Byeon <dongb94@gmail.com>
2022-06-08 16:36:21 +0900

    update html

--- build/index.js
+++ build/index.js
@@ -13,6 +13,69 @@ app.get('/', function(request, response){
     response.sendFile(__dirname+'/public/index.html');
 });
 
+app.get('/public/html/get_draft', function(request, response){
+    result = DB.query(`SELECT 'number', 'title', 'text', 'owner', 'time', 'views' FROM 'board' WHERE 1`);
+    console.log(result);
+    var res =
+    `
+    <html style="height: 100%">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/bootstrap.css">
+        
+        <script src="../js/jquery-3.6.0.js"></script>
+        <script src="../js/bootstrap.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <table class="table table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th style="width: 10%;">번호</th>
+                        <th style="width: 50%;">제목</th>
+                        <th>작성자</th>
+                        <th>조회수</th>
+                        <th>날짜</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22</td>
+                        <td><a href="read.php" class="text-reset">2235252353</a></td>
+                        <td>관리자</td>
+                        <td>2021-01-02</td>
+                        <td>20</td>
+                    </tr>
+                </tbody>
+            </table>
+            <hr />
+            <div class="container">
+                <div class="btn-toolbar row justify-content-md-evenly" role="toolbar" aria-label="Toolbar with button groups">
+                    <div class="btn-group mr-2 col-4" role="group" aria-label="First group">
+                        <button type="button" class="btn btn-secondary">&lt;</button>
+                        <button type="button" class="btn btn-secondary">1</button>
+                        <button type="button" class="btn btn-secondary">2</button>
+                        <button type="button" class="btn btn-secondary">3</button>
+                        <button type="button" class="btn btn-secondary">4</button>
+                        <button type="button" class="btn btn-secondary">5</button>
+                        <button type="button" class="btn btn-secondary">&gt;</button>
+                    </div>
+                </div>
+                <div class="row justify-content-end" style="margin-top: 10px;">
+                    <a class="btn btn-dark col-2" href="./write_board.html">글쓰기</a>
+                </div>
+            </div>
+
+        </div>
+    </body>
+    </html>
+    `;
+
+    response.send(res);
+});
+
 app.use(express.static(`public`));
 
 app.listen(process.env.PORT || 5000, ()=>{
--- build/public/js/get_board.js
+++ build/public/js/get_board.js
@@ -1,12 +1,5 @@
 var DB = require("../DB/mysql");
 
-console.log(result);
-
-onload(
-    ()=>{console.log(result);}
-);
-
-
 exports.readTable = function(){
     return DB.query(`SELECT 'number', 'title', 'text', 'owner', 'time', 'views' FROM 'board' WHERE 1`);
 }
