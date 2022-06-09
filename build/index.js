99e68e280860ef84d9e6ca3c6eb5617967b556c0
Donggeon Byeon <dongb94@gmail.com>
2022-06-09 21:35:33 +0900

    no message

--- build/index.js
+++ build/index.js
@@ -178,7 +178,7 @@ function readPost(page, postNum, response)
                     <meta charset="utf-8" />
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                     <!-- Core theme CSS (includes Bootstrap)-->
-                    <link href="../css/bootstrap.css" rel="stylesheet" />
+                    <link href="/css/bootstrap.css" rel="stylesheet" />
                 </head>
                 <body>
                     <!-- Page content-->
