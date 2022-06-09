5c2b26f0314cff3caa8d91e3ac2000eaac2bfabd
Donggeon Byeon <dongb94@gmail.com>
2022-06-09 10:30:42 +0900

    no message

--- build/DB/mysql.js
+++ build/DB/mysql.js
@@ -1,33 +1,37 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
   host     : '13.209.67.44',
   user     : 'centos',
   password : 'art3640#',
   database : 'arterior'
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
 
-  console.log('connected as id ' + connection.threadId);
-});
+//   console.log('connected as id ' + connection.threadId);
+// });
 
-exports.query = function(queryText){
-    var lResult;
-    var lField;
-    connection.query(queryText, function (error, results, fields) {
-        if (error) throw error;
 
-        lResult = results;
-        lField = fields;
-
-        console.log("SQL query Success");
-        // console.log("RESUILT : "+result);
-        // console.log("FIELD : "+fields);
-    });
-
-    return lResult;
-} 
+exports.executeQuery = function(query, callback) {
+  pool.getConnection(function (err, connection) {
+    if (err) {
+        return callback(err, null);
+    }
+    else if (connection) {
+        connection.query(query, function (err, rows, fields) {
+            connection.release();
+            if (err) {
+                return callback(err, null);
+            }
+            return callback(null, rows);
+        })
+    }
+    else {
+        return callback(true, "No Connection");
+    }
+  });
+}
--- build/index.js
+++ build/index.js
@@ -17,77 +17,85 @@ app.get('/', function(request, response){
 
 
 app.get('/html/board/:dyn', function(request, response){
-    var result = DB.query(`SELECT 'number', 'title', 'text', 'owner', 'time', 'views' FROM board`);
-    console.log(result);
-    var num = request.params.dyn*10<result.length?10:result.length/10;
-    var res =
-    `
-    <html style="height: 100%">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/css/bootstrap.css">
+    DB.executeQuery(`SELECT 'number', 'title', 'text', 'owner', 'time', 'views' FROM board`, (err, rows)=>{
+        if(!err)
+        {
+            console.log(rows);
+            var num = request.params.dyn*10<rows.length?10:rows.length/10;
+            var res =
+            `
+            <html style="height: 100%">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="/css/bootstrap.css">
+                
+                <script src="/js/jquery-3.6.0.js"></script>
+                <script src="/js/bootstrap.js"></script>
+            </head>
         
-        <script src="/js/jquery-3.6.0.js"></script>
-        <script src="/js/bootstrap.js"></script>
-    </head>
-
-    <body>
-        <div class="container">
-            <table class="table table-striped">
-                <thead class="thead-dark">
-                    <tr>
-                        <th style="width: 10%;">번호</th>
-                        <th style="width: 50%;">제목</th>
-                        <th>작성자</th>
-                        <th>조회수</th>
-                        <th>날짜</th>
-                    </tr>
-                </thead>
-                <tbody>`;
-
-                for(var i=0; i<num; i++)
-                {
-                    res += `
-                    <tr>
-                        <td>${result[i].number}</td>
-                        <td><a href="read.php" class="text-reset">${result[i].title}</a></td>
-                        <td>${result[i].owner}</td>
-                        <td>${result[i].time}</td>
-                        <td>${result[i].views}</td>
-                    </tr>`;
-                }
-                
-                
-    res += `
-                </tbody>
-            </table>
-            <hr />
-            <div class="container">
-                <div class="btn-toolbar row justify-content-md-evenly" role="toolbar" aria-label="Toolbar with button groups">
-                    <div class="btn-group mr-2 col-4" role="group" aria-label="First group">
-                        <button type="button" class="btn btn-secondary">&lt;</button>`;
-                        for(var i=0; i<result.length; i+=10)
+            <body>
+                <div class="container">
+                    <table class="table table-striped">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th style="width: 10%;">번호</th>
+                                <th style="width: 50%;">제목</th>
+                                <th>작성자</th>
+                                <th>조회수</th>
+                                <th>날짜</th>
+                            </tr>
+                        </thead>
+                        <tbody>`;
+        
+                        for(var i=0; i<num; i++)
                         {
-                            res+=`
-                            <button type="button" class="btn btn-secondary" id="${(i/10)+1}">${(i/10)+1}</button>
-                            `;
+                            res += `
+                            <tr>
+                                <td>${rows[i].number}</td>
+                                <td><a href="read.php" class="text-reset">${rows[i].title}</a></td>
+                                <td>${rows[i].owner}</td>
+                                <td>${rows[i].time}</td>
+                                <td>${rows[i].views}</td>
+                            </tr>`;
                         }
-    res+=`
-                        <button type="button" class="btn btn-secondary">&gt;</button>
+                        
+                        
+            res += `
+                        </tbody>
+                    </table>
+                    <hr />
+                    <div class="container">
+                        <div class="btn-toolbar row justify-content-md-evenly" role="toolbar" aria-label="Toolbar with button groups">
+                            <div class="btn-group mr-2 col-4" role="group" aria-label="First group">
+                                <button type="button" class="btn btn-secondary">&lt;</button>`;
+                                for(var i=0; i<rows.length; i+=10)
+                                {
+                                    res+=`
+                                    <button type="button" class="btn btn-secondary" id="${(i/10)+1}">${(i/10)+1}</button>
+                                    `;
+                                }
+            res+=`
+                                <button type="button" class="btn btn-secondary">&gt;</button>
+                            </div>
+                        </div>
+                        <div class="row justify-content-end" style="margin-top: 10px;">
+                            <a class="btn btn-dark col-2" href="./write_board.html">글쓰기</a>
+                        </div>
                     </div>
+        
                 </div>
-                <div class="row justify-content-end" style="margin-top: 10px;">
-                    <a class="btn btn-dark col-2" href="./write_board.html">글쓰기</a>
-                </div>
-            </div>
-
-        </div>
-    </body>
-    </html>
-    `;
-
-    response.send(res);
+            </body>
+            </html>
+            `;
+        
+            response.send(res);
+        }
+        else
+        {
+            console.log(err);
+        }
+    });
 });
 
 app.use(express.static(`public`));
